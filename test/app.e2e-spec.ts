d037f6625c9e9c32e62face7fdf07d68aeccb7ef
Sumit <sumit.singh@neosofttech.com>
2021-08-17 09:22:22 +0530

    test(test): Created test cases for guard and logger

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import coreBootstrap from '@libs/core/bootstrap';
-import { Response } from '@libs/core';
+import { users } from './users.response';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -22,8 +22,23 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 
-  it('should return an hello world string along with status', async () => {
+  it('Should return an hello world string along with status', async () => {
     const { body }: any = await request(app.getHttpServer()).get('').expect(200);
     expect(body.data).toEqual('Hello World!');
+  });
+  it('Should return users array along with 200 status code', async () => {
+    const response = await request(app.getHttpServer()).get('/users').expect(200);
+    expect(response.status).toEqual(200);
+    expect(response.body.data).toEqual(users);
+  });
+  it('Should return 201 created status along with success message', async () => {
+    const response = await request(app.getHttpServer()).post('/users').send({
+      "id": 30,
+      "firstName": "Rohit",
+      "lastName": "Sharma",
+      "isActive": true
+    })
+    expect(response.status).toBe(201);
+    expect(response.body.data).toBe('success');
   });
 });
---
+++ test/users.response.ts
@@ -0,0 +1,29 @@
+export const users = [{
+    "id": 1,
+    "firstName": "Sumit",
+    "lastName": "Singh",
+    "isActive": true
+  },
+  {
+    "id": 2,
+    "firstName": "Prajwal",
+    "lastName": "Karale",
+    "isActive": true
+  },
+  {
+    "id": 3,
+    "firstName": "Rohit",
+    "lastName": "Sharma",
+    "isActive": true
+  },
+  {
+    "id": 4,
+    "firstName": "Virat",
+    "lastName": "Kohli",
+    "isActive": true
+  },{
+    "id": 30,
+    "firstName": "Rohit",
+    "lastName": "Sharma",
+    "isActive": true
+  }]
